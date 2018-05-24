ea26afdc7ad5af7c179039180b2e24877a369ba2
Mark Hourani <mhourani@flexera.com>
2018-05-23 17:30:50 -0700

    Added condition based on if main Initial commit for ny_baby_names.py

--- monarch_script.py
+++ monarch_script.py
@@ -3,36 +3,38 @@
 
 #Import json from monarch file
 import json, urllib.request
-with urllib.request.urlopen("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json") as jsonFile:
-    jsonContent = json.loads(jsonFile.read().decode())
 
-'''
-Note: the instructions did not say to sort so I did not
+if __name__ == "__main__":
+	with urllib.request.urlopen("http://mysafeinfo.com/api/data?list=englishmonarchs&format=json") as jsonFile:
+	    jsonContent = json.loads(jsonFile.read().decode())
 
-if you want to sort the groups you coudl do the following:
-houses = sorted(list(set([x['hse'] for x in jsonContent])))
-cities = sorted(list(set([x['cty'] for x in jsonContent])))
-'''
+	'''
+	Note: the instructions did not say to sort so I did not
 
-#create a set of the houses
-houses = list(set([x['hse'] for x in jsonContent]))
+	if you want to sort the groups you coudl do the following:
+	houses = sorted(list(set([x['hse'] for x in jsonContent])))
+	cities = sorted(list(set([x['cty'] for x in jsonContent])))
+	'''
 
-#create a set of the cities
-cities = list(set([x['cty'] for x in jsonContent]))
+	#create a set of the houses
+	houses = list(set([x['hse'] for x in jsonContent]))
+
+	#create a set of the cities
+	cities = list(set([x['cty'] for x in jsonContent]))
 
 
-result = {}
-clist = set()
-house_dict = {}
+	result = {}
+	clist = set()
+	house_dict = {}
 
-for c in cities:
-    for h in houses:
-        names = [d['nm'] for d in jsonContent if d['cty'] == c and d['hse'] == h]
-        house_dict[h] = names
-    result[c] = house_dict     
+	for c in cities:
+	    for h in houses:
+	        names = [d['nm'] for d in jsonContent if d['cty'] == c and d['hse'] == h]
+	        house_dict[h] = names
+	    result[c] = house_dict     
 
-#We could sort the names if we wanted
-json_string=json.dumps(result, indent=0, sort_keys=False)
+	#We could sort the names if we wanted
+	json_string=json.dumps(result, indent=0, sort_keys=False)
 
-print(json_string)
+	print(json_string)
 
